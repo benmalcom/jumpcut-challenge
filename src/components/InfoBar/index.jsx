5ab6449472750bf7da6a89ed22d5ef13edb88617
Benjamin Ikeji <benjaminikeji@gmail.com>
2018-12-29 17:16:21 +0100

    Added functional InfoBar component

---
+++ src/components/InfoBar/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Badge, Jumbotron } from 'reactstrap';
+import PropTypes from 'prop-types';
+
+const InfoBar =  ({ sequencer }) => {
+	return (<div>
+		<Jumbotron>
+			<h1>Sequencer Generator</h1>
+			{sequencer ? (<div>
+				Current Sequencer: <Badge pill className="m-1" color="info">{sequencer.label}</Badge>
+			</div>) : <p className="lead">Hi, choose a sequencer in the left sidebar to see the output</p>}
+		</Jumbotron>
+	</div>);
+};
+InfoBar.propTypes = {
+	sequencer: PropTypes.object.isRequired,
+};
+export default InfoBar;
